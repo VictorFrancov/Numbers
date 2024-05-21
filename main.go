05c3c14ac7845f23cb798854ccf45120924ec1f2
VICTOR GABRIEL FRANCO VIEIRA <victor.vieira@nbsi.com.br>
2024-05-20 20:45:14 -0400

    Criado metodos para resolver problemas numericos

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Primeira Parte")
+	for numero := 1; numero <= 100; numero++ {
+		if numero%3 == 0 {
+			fmt.Println(numero)
+		}
+	}
+	fmt.Println("----------------------------------")
+	fmt.Println("//////////////////////////////////")
+	fmt.Println("----------------------------------")
+	fmt.Println("Segunda Parte")
+
+	for numero := 1; numero <= 100; numero++ {
+		if numero%3 == 0 {
+			fmt.Println("PIN")
+		} else if numero%5 == 0 {
+			fmt.Println("PAN")
+
+		}
+	}
+}
